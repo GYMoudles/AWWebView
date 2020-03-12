2394d89c0bb5f51bbdfb2dc9a77865b68c324ad6
zgy <173678978@qq.com>
2020-03-12 13:05:29 +0800

    no message

--- AWWebView/Classes/AWWebViewController.h
+++ AWWebView/Classes/AWWebViewController.h
@@ -9,12 +9,21 @@
 #import <WebKit/WebKit.h>
 #import <WebViewJavascriptBridge/WKWebViewJavascriptBridge.h>
 
+
+
 NS_ASSUME_NONNULL_BEGIN
 
 
 extern NSString *const kJSHandleFunctionName;/// js端 注册的方法名称
 
+
+#if __has_include(<AWBaseSDK/AWBaseViewController.h>)
+#import <AWBaseSDK/AWBaseViewController.h>
+@interface AWWebViewController : AWBaseViewController
+#else
 @interface AWWebViewController : UIViewController
+#endif
+
 
 @property (nonatomic, strong) WKWebView *webView;
 
